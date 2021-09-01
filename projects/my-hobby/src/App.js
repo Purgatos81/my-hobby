15041488525c150f8649067f25e87f8aa481c842
Purgatos81 <Johnhc81@hotmail.com>
2021-09-01 13:51:53 +0200

    feat: basic work

--- projects/my-hobby/src/App.js
+++ projects/my-hobby/src/App.js
@@ -1,15 +1,23 @@
 import HeaderComp from "./components/header/Header";
+import FooterComp from "./components/footer/Footer";
 
 import "./sass/main.scss";
+import image from "./media/pubg.jpg";
 
 
 const App = () => {
   return (
-    <div className="wrapper">
-    <>
-      <HeaderComp />
-    </>
-    </div>
+    <body className="wrapper">
+      <header>
+        <HeaderComp />
+      </header>
+      <main>
+      <img src={image} />
+      </main>
+      <footer>
+        <FooterComp />
+      </footer>
+    </body>
   )
 }
 
---
+++ projects/my-hobby/src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import { wrapper } from "./footer.module.scss";
+
+const FooterComp = props => {
+    return (
+        <div className={wrapper}>
+            <p>©John H Christiansen</p>
+        </div>
+    )
+}
+
+export default FooterComp;
