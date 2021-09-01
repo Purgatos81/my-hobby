6df16ec18606f6a13fb6e980fe81a53932ffb05e
Purgatos81 <Johnhc81@hotmail.com>
2021-09-01 14:43:15 +0200

    feat: added text and color variables

--- projects/my-hobby/src/App.js
+++ projects/my-hobby/src/App.js
@@ -3,6 +3,7 @@ import FooterComp from "./components/footer/Footer";
 
 import "./sass/main.scss";
 import image from "./media/pubg.jpg";
+import { TextCollection } from "./utils/Texts";
 
 
 const App = () => {
@@ -13,6 +14,9 @@ const App = () => {
       </header>
       <main>
       <img src={image} />
+      <p>{TextCollection.text1}</p>
+      <p>{TextCollection.text2}</p>
+      <p>{TextCollection.text3}</p>
       </main>
       <footer>
         <FooterComp />
---
+++ projects/my-hobby/src/utils/Texts.js
@@ -0,0 +1,5 @@
+export const TextCollection = {
+    text1: "Text from one",
+    text2: "Text from two",
+    text3: "Text from tree"
+}
